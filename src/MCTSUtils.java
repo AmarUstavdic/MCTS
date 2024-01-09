e3d1f0055356bfa1ceb55829b2ae62e1888c27f7
lilwizzz <amar.ustavdija@gmail.com>
2024-01-10 00:25:52 +0100

    separated utils in separate class

--- src/MCTSUtils.java
+++ src/MCTSUtils.java
@@ -1,2 +1,10 @@
 public class MCTSUtils {
+
+
+    public static int depth(MCTSNode mctsNode) {
+        return (mctsNode == null) ? 0 :
+                (mctsNode.getChildren().isEmpty()) ? 1 :
+                        1 + mctsNode.getChildren().stream().mapToInt(MCTSUtils::depth).max().orElse(0);
+    }
+
 }
